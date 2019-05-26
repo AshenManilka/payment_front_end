9ead603be4efc409b8471639885d8fa84fd157f7
AshenManilka <ashen9593@gmail.com>
2019-05-26 19:15:46 +0530

    complete

---
+++ validation_del.js
@@ -0,0 +1,18 @@
+
+     
+     function validation(){
+                            var cardid = document.getElementById('cardid').value;
+                           
+                           
+                           
+                           
+                            
+                            if( cardid == ""){
+                              document.getElementById('cardidl').innerHTML = " ** please fill the CARDID";
+                              return false;
+                            }
+                           
+     }
+                            
+
+
